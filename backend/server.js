417b51b6fdcc70c412ff0a9c452cc85b3f6330b0
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-17 22:27:13 -0500

    require path

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path');
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
