eb1aa095a0a8f72fee53f09cb1edabf325346d00
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-18 05:13:25 -0500

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,38 +6,39 @@ const path = require('path');
 const workoutRoutes = require('./routes/workouts');
 const userRoutes = require('./routes/user');
 
-// express app
+// Express app
 const app = express();
 
-app.set('port', process.env.PORT || 5000);
-console.log("+++++++++++++++" + app.get('port'));
-
-const staticPath = path.join(__dirname, '..', 'frontend', 'build');
-console.log('Static files path:', staticPath);
+// Set port
+const port = process.env.PORT || 5000;
+app.set('port', port);
+console.log('Server is running on port:', port);
 
 // Serve static files for React build
+const staticPath = path.join(__dirname, '..', 'frontend', 'build');
+console.log('Static files path:', staticPath);
 app.use(express.static(staticPath));
 
-// middleware
+// Middleware
 app.use(express.json());
 
 // Serve React index.html for all routes
-app.get("*", (req, res) => {
+app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
 });
 
-// routes
+// Routes
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
 
-// connect to db
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
 	.then(() => {
-		// listen for requests
-		app.listen(process.env.PORT, () => {
-			console.log('connected to db & listening on port', process.env.PORT);
+		// Listen for requests
+		app.listen(port, () => {
+			console.log('Connected to the database. Server is listening on port:', port);
 		});
 	})
 	.catch((error) => {
-		console.log(error);
+		console.error('Error connecting to MongoDB:', error);
 	});
