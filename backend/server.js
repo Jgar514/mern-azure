7ee74da729c2cad09089e859cf42ed7037f0be09
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-17 21:54:59 -0500

    undid server.js changes

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,33 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
-const mongoose = require("mongoose");
-const workoutRoutes = require("./routes/workouts");
-const userRoutes = require("./routes/user");
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
 // express app
-const app = express();
+const app = express()
 
 // middleware
-app.use(express.json());
+app.use(express.json())
 
 app.use((req, res, next) => {
-	console.log(req.path, req.method);
-	next();
-});
-
-// Serve static files for React build
-app.use(express.static("frontend/build"));
+	console.log(req.path, req.method)
+	next()
+})
 
 // routes
-app.use("/api/workouts", workoutRoutes);
-app.use("/api/user", userRoutes);
+app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
-// Connect to the database
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
 	.then(() => {
-		console.log("Connected to the database");
-
-		// Listen to port
-		const port = process.env.PORT || 5000;
-		app.listen(port, () => {
-			console.log("Listening for requests on port", port);
-		});
+		// listen for requests
+		app.listen(process.env.PORT, () => {
+			console.log('connected to db & listening on port', process.env.PORT)
+		})
 	})
-	.catch((err) => {
-		console.error("Error connecting to the database:", err);
-	});
+	.catch((error) => {
+		console.log(error)
+	})
