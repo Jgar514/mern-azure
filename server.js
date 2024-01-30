7829308b334a7552067ae8b4dcdaafaf26bf73cb
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-30 05:13:38 -0500

    changing client to frontend

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 
 // Serve Static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("fontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "fontend", "build", "index.html"));
   });
 }
 
