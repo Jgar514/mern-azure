7d90684e914861cc33b44613fe08c35edd0082e8
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-30 01:16:00 -0500

    gitignore

--- server.js
+++ server.js
@@ -6,36 +6,37 @@ const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const path = require("path"); // on top
 
-// Serve Static assests if in production
+// express app
+const app = express();
+
+// Serve Static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build")); // change this if your dir structure is different
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-// express app
-const app = express()
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // routes
-app.use('/api/workouts', workoutRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
