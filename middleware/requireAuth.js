b72e6457380b05e381c023a2b2c0ce271c686703
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-30 00:47:28 -0500

    out of backend

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -6,7 +6,7 @@ const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers
 
   if (!authorization) {
-    return res.status(401).json({error: 'Authorization token required'})
+    return res.status(401).json({ error: 'Authorization token required' })
   }
 
   const token = authorization.split(' ')[1]
@@ -19,7 +19,7 @@ const requireAuth = async (req, res, next) => {
 
   } catch (error) {
     console.log(error)
-    res.status(401).json({error: 'Request is not authorized'})
+    res.status(401).json({ error: 'Request is not authorized' })
   }
 }
 
