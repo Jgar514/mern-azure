c812e99faa99e7f7f0f9d45767af195baeb19510
Joshua Garvey <j.garvey514@yahoo.com>
2024-01-30 22:51:17 -0500

    replaced server.js accident

--- server.js
+++ server.js
@@ -1,34 +1,48 @@
-import { createContext, useReducer } from 'react'
+require('dotenv').config();
 
-export const WorkoutsContext = createContext()
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+const path = require("path");
 
-export const workoutsReducer = (state, action) => {
-  switch (action.type) {
-    case 'SET_WORKOUTS':
-      return {
-        workouts: action.payload
-      }
-    case 'CREATE_WORKOUT':
-      return {
-        workouts: [action.payload, ...state.workouts]
-      }
-    case 'DELETE_WORKOUT':
-      return {
-        workouts: state.workouts.filter((w) => w._id !== action.payload._id)
-      }
-    default:
-      return state
-  }
-}
+// express app
+const app = express();
 
-export const WorkoutsContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(workoutsReducer, {
-    workouts: null
+// Set the port for the server to either the process environment's PORT variable or 5000
+app.set('port', process.env.PORT || 5000);
+console.log("+++++++++++++++" + app.get('port'));
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use(express.static('./client/build'));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to db & listening on port', process.env.PORT);
+    });
   })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
-  return (
-    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
-      {children}
-    </WorkoutsContext.Provider>
-  )
-}
+module.exports = app;
